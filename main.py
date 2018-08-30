63c183ed52f8f2a2a1fa77ed747ef9e83e64f720
Alvaro Torres <torrestal@gmail.com>
2018-08-30 11:55:27 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+import pygame
+
+if __name__ == "__main__":
+    if sys.platform == 'win32' or sys.platform == 'win64':
+        os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+    pygame.init()
+    reloj = pygame.time.Clock()
+
+    resolution = (1050, 600)
+    pygame.display.set_caption('Bolas')
+    screen = pygame.display.set_mode(resolution, pygame.SRCALPHA, 32)
+
+    done = False
+    fps = 20
+    count = 9999
+
+    while not done:
+        screen.fill((0, 0, 0, 255))
+        events = pygame.event.get()
+        keys = pygame.key.get_pressed()
+        mouse = pygame.mouse.get_pressed()
+
+        # count += 1
+        # Once a minute:
+        # if count > fps * 20:
+        #     count = 0
+
+        for event in events:
+            if event.type == pygame.QUIT:
+                done = True
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            # if event.type == pygame.KEYDOWN and keys[pygame.K_c]:
+            # if event.type == pygame.KEYDOWN and keys[pygame.K_s]:
+            # if event.type == pygame.KEYDOWN and keys[pygame.K_t]:
+            # if event.type == pygame.KEYDOWN and keys[pygame.K_TAB]:
+        if keys[pygame.K_ESCAPE]:
+            done = True
+
+        pygame.display.flip()
+        reloj.tick(fps)
+
+    # Cerramos la ventana y salimos.
+    # Si te olvidas de esta ultima linea, el programa se 'colgara'
+    # al salir si lo hemos estado ejecutando desde el IDLE.
+    pygame.quit()
