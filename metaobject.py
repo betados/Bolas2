28cd6cd3ddd933ebdf39d93d239788d0268af6a4
Alvaro Torres <torrestal@gmail.com>
2018-11-19 13:02:13 +0100

    movement of metaobject starting to work

--- metaobject.py
+++ metaobject.py
@@ -16,24 +16,29 @@ class Metaobject(RigidBody):
 
 class Car(Metaobject):
     def __init__(self, pos):
-        self.body = RectBody((0, 0, 20, 10))
-        self.wheel = RoundBody((0, 10), 2, affected_by_gravity=False)
-        self.wheel2 = RoundBody((20, 10), 2, affected_by_gravity=False)
+        
+        self.body = RectBody((*pos, 20, 10))
+        self.wheel = RoundBody(Vector(*pos) + Vector(0, 10), 2, affected_by_gravity=False)
+        self.wheel2 = RoundBody(Vector(*pos) + Vector(20, 10), 2, affected_by_gravity=False)
         Metaobject.__init__(self, self.body, self.wheel, self.wheel2)
 
     def draw(self, screen):
         # body
         pygame.draw.polygon(screen,
                             (0, 50, 0),
-                            [[p.x + self.body._pos.x + self._pos.x, p.y + self.body._pos.y + self._pos.y] for p in
+                            [[p.x + self.body._pos.x, p.y + self.body._pos.y] for p in
                              self.body.points], 0
                             )
         # wheels
-        pygame.draw.circle(screen, (255, 20, 20), (self.wheel.pos + self._pos).int(), self.wheel.radio)
-        pygame.draw.circle(screen, (20, 20, 20), (self.wheel2.pos + self._pos).int(), self.wheel2.radio)
+        pygame.draw.circle(screen, (255, 20, 20), self.wheel.pos.int(), self.wheel.radio)
+        pygame.draw.circle(screen, (20, 20, 20), self.wheel2.pos.int(), self.wheel2.radio)
         # print self.wheel._forces
 
     def actualize(self, time):
-        pass
-        # for obj in self:
-            # obj.actualize(time)
+        pos_ant = self._pos
+        for obj in self:
+            self._forces += obj._forces
+        RigidBody.actualize(self, time)
+        dif = self._pos - pos_ant
+        for obj in self:
+            obj._pos += dif
